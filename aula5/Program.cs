c84676d38654eb6c93cccd6d40c670882b7cd110
Juliano Marques <julianomt456@gmail.com>
2026-09-22 22:41:53 -0300

    Aula 05 Operadores e Operadores aritméticos

---
+++ aula5/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Aula05
+{
+    static void Main()
+    {
+        // Aprendizado de operadores, já aprendido anteriormente em aula de lógica de programação, então pulei esssa parte!!! Sim, aprendi em C#
+    }
+}
