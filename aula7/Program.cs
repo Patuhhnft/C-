54c4d2566301ede326f12fdcaa44ef915420550b
Juliano Marques <julianomt456@gmail.com>
2026-09-22 23:28:19 -0300

    Estudo de constantes

---
+++ aula7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+
+class Aula07
+{
+    static void Main()
+    {
+        const string canal = "CFB Cursos"; 
+        const double pi = 3.1415;
+
+        Console.WriteLine("Canal {0}\nPi: {1}", canal, pi); 
+    }
+}
